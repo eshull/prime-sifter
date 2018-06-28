745fef720a0faf61eb4ca52c36d8e7963ceeb72c
Eric Shull and Nate Cottle <nate@>
2018-06-28 14:00:35 -0700

    Add second passing test and script returns prime numbers up to user input.

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -2,10 +2,15 @@ require('pry')
 class NumberSifting
   def initialize(input)
     @number = input
+
   end
-
   def sifter
-    answer_array = (2..@number).reject { |i|  i % 2 == 0 }
-
+    number_array = (2..@number).to_a
+    index = 0
+    while index < (number_array.length - 1) do
+      number_array = (number_array.reject { |i|  i % number_array[index] == 0 unless i == number_array[index]})
+      index += 1
+    end
+  number_array
   end
 end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -4,6 +4,10 @@ require('rspec')
 describe('#prime_sifting') do
   it('removes multiples of 2 from array')do
   number = NumberSifting.new(12)
-    expect(number.sifter).to(eq([3,5,7,9,11]))
+    expect(number.sifter).to(eq([2,3,5,7,11]))
+  end
+  it('removes multiples of 2 from array')do
+  number = NumberSifting.new(100)
+    expect(number.sifter).to(eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]))
   end
 end
